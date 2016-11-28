3a7dc1f7c0c579c3014da7d1ee109dc817b09876
Taxxon <te15.emil.kack@umea.nti.se>
2016-11-28 11:41:11 +0100

    Rejected commit: Default

--- src/PRG1Uppgift1.java
+++ src/PRG1Uppgift1.java
@@ -1,11 +1,34 @@
+import javax.swing.*;
+
 /**
  * Created by Emil Käck on 2016-11-28.
  */
+
 public class PRG1Uppgift1 {
     public static void main(String[] arg){
+/**
+        String Numbers= "";
+        int rows=9;
 
-        
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++)
+                Numbers = Numbers + " " + i*j;
+                Numbers = Numbers + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, Numbers);
+**/
+/** Assignment 5.5 Page 122 **/
+
+        double absolut = 0.00001;
+        double i;
+
+        for(i=1; i<=absolut; i++){
+
+
+        }
 
     }
 
 }
+
