8f531224cecf242607e4e3b44b7154886534e37a
Taxxon <te15.emil.kack@umea.nti.se>
2016-12-19 11:01:30 +0100

    Klar med fråga 8.2 och stöte inte på några stora problem

--- src/Metoder.java
+++ src/Metoder.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 /**
  * Created by Emil Käck on 2016-12-19.
  */
 public class Metoder {
+    public static void main(String[] arg){
+        // Tar reda på kostan utan moms och momsen och gör om det till en double sen anropar jag metoden uppgift 8.2 sida 174
+        /**
+        String Pris;
+        Pris = JOptionPane.showInputDialog("Kostnad utan moms?");
+        double Utanmoms = Double.parseDouble(Pris);
+        Pris = JOptionPane.showInputDialog("Vad är momsen i procent?");
+        double Moms = Double.parseDouble(Pris);
+        double Medmoms = kostnad(Utanmoms, Moms);
+        JOptionPane.showMessageDialog(null, "Kostnaden med moms = " + Medmoms);
+        **/
+
+        
+
+    }
+    // Metod för att beräkna kostnaden med moms i procent
+    public static double kostnad (double a, double b) {
+        return a*(b/100+1);
+    }
 }
